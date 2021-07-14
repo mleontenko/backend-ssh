97a43cd1351fdedd1f0f0f6936d0ca6b08070cbe
Marin Leontenko <marin.leontenko@gmail.com>
2021-07-14 16:09:17 +0200

    paramiko return ls command result from remote ssh

--- api.py
+++ api.py
@@ -1,15 +1,28 @@
 import flask
 from flask import request
+import paramiko
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/', methods=['POST'])
 def home():
-    ip = request.form["ip"]
+    host = request.form["ip"]
     username = request.form["username"]
     password = request.form["password"]
+    port = 22
+
+    command = "ls"
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, port, username, password)
+
+    stdin, stdout, stderr = ssh.exec_command(command)
+    lines = stdout.readlines()
+    lines=str(lines)
+    #print(lines)
     
-    return ip
+    return lines
 
 app.run()
