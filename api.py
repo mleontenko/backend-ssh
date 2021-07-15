a9bedb2d2214327a1d4134a46b74eab7bdf76b0a
Marin Leontenko <marin.leontenko@gmail.com>
2021-07-15 22:16:38 +0200

    execute ps aux on remote, parse output, return json

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 import flask
 from flask import request
 import paramiko
+import pprint
+import subprocess
+from flask import json
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -12,17 +15,26 @@ def home():
     password = request.form["password"]
     port = 22
 
-    command = "ls"
+    command = "ps aux "
 
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host, port, username, password)
 
     stdin, stdout, stderr = ssh.exec_command(command)
-    lines = stdout.readlines()
-    lines=str(lines)
-    #print(lines)
-    
-    return lines
+    output = stdout.readlines()    
+    #lines=str(lines)
+
+    headers = [h for h in ' '.join(output[0].strip().split()).split() if h]
+    raw_data = map(lambda s: s.strip().split(None, len(headers) - 1), output[1:])
+    data = [dict(zip(headers, r)) for r in raw_data]
+
+    print(data)
+
+    response = app.response_class(
+        response=json.dumps(data),
+        mimetype='application/json'
+    )
+    return response
 
 app.run()
